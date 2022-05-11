488fc8de5ee2c764243f3a0dd3ee067dcf256655
Fazry <lhuqita.fazry@gmail.com>
2022-05-11 09:20:15 +0700

    adding resize

---
+++ resize.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import cv2
+from argparse import ArgumentParser
+from glob import glob
+
+if __name__ == '__main__':
+    # ------------
+    # args
+    # ------------
+    parser = ArgumentParser()
+    parser.add_argument('--dataset_root', type=str)
+
+    args = parser.parse_args()
+    dataset_root = args.dataset_root
+
+    for f2 in ['A', 'B', 'label']:
+        print(f'Processing {f2}')
+
+        for fi1 in glob(os.path.join(dataset_root, f2, '*')):
+            img = cv2.imread(fi1)
+            resized = cv2.resize(img, (128, 128))
+            cv2.imwrite(fi1, resized)
